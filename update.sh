6459a78e256f7fb8aa640b29cf7092cce43d50f5
Ezequiel Biavaschi <jbezequiel@gmail.com>
2023-10-15 13:03:30 +0200

    Revert to EOL versions

--- update.sh
+++ update.sh
@@ -1,7 +1,53 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+# TODO scrape this somehow?
+supportedVersions=(
+	15
+	14
+	13
+	12
+	11
+	10
+	9.6
+	#9.5
+	#9.4
+	#9.3
+	#9.2
+)
+suite='bookworm'
 
-./versions.sh "$@"
-./apply-templates.sh "$@"
+for i in "${!supportedVersions[@]}"; do
+	new="${supportedVersions[$i]}"
+	echo "# $new"
+	docker pull "postgres:$new-$suite" > /dev/null
+	(( j = i + 1 ))
+	for old in "${supportedVersions[@]:$j}"; do
+		dir="$old-to-$new"
+		echo "- $old -> $new ($dir)"
+		oldVersion="$(
+			docker run --rm -e OLD="$old" "postgres:$new-$suite" bash -Eeuo pipefail -c '
+				sed -i "s/\$/ $OLD/" /etc/apt/sources.list.d/pgdg.list
+				apt-get update -qq 2>/dev/null
+				apt-cache policy "postgresql-$OLD" \
+					| awk "\$1 == \"Candidate:\" { print \$2; exit }"
+			'
+		)"
+		echo "  - $oldVersion"
+		if [ "$oldVersion" = '(none)' ]; then
+			continue
+		fi
+		mkdir -p "$dir"
+		sed \
+			-e "s!%%POSTGRES_OLD%%!$old!g" \
+			-e "s!%%POSTGRES_OLD_VERSION%%!$oldVersion!g" \
+			-e "s!%%POSTGRES_NEW%%!$new!g" \
+			-e "s!%%SUITE%%!$suite!g" \
+			Dockerfile.template \
+			> "$dir/Dockerfile"
+		cp docker-upgrade "$dir/"
+		if [[ "$old" != 9.* ]]; then
+			sed -i -e '/postgresql-contrib-/d' "$dir/Dockerfile"
+		fi
+	done
+done
